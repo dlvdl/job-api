7e3ad4e318c6e1a7cba405158440d371ae07d418
DenysLygach <denisligach98@gmail.com>
2023-06-30 14:41:48 +0300

    user schema created

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 require("express-async-errors")
 const express = require("express")
+const connectDB = require("./db/connect")
 const app = express()
 
 // connectDB
@@ -27,6 +28,8 @@ const port = process.env.PORT || 3000
 
 const start = async () => {
   try {
+    await connectDB(process.env.DB_URL)
+    console.log("DB_connected!")
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     )
