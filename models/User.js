685cd22793602017191194be0619fea6e9e41f6c
DenysLygach <denisligach98@gmail.com>
2023-06-30 16:19:57 +0300

    done with register controller

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -25,4 +27,17 @@ const UserSchema = new mongoose.Schema({
   },
 })
 
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10)
+  // we talking with document here
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
+// There is a possibility to create custom method for our document.
+// So instead writing logic of signing of jwt token in controller I`ll write it here.
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id, name: this.name }, "jwtSecret", {
+    expiresIn: "30d",
+  })
+}
 module.exports = mongoose.model("User", UserSchema)
