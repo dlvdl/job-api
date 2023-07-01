0a9f5086f489592cf9ff89787beb52fa6788d627
DenysLygach <denisligach98@gmail.com>
2023-07-01 15:57:24 +0300

    done with login and register controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,39 @@
 const User = require("../models/User")
 const { StatusCodes } = require("http-status-codes")
-const { BadRequestError } = require("../errors")
+const { BadRequestError, UnauthenticatedError } = require("../errors")
 
 const register = async (req, res, next) => {
   const user = await User.create({ ...req.body })
+  // Here I use the method that I creates in model
   const token = user.createJWT()
-
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
 const login = async (req, res, next) => {
-  res.send("login user")
+  const { email, password } = req.body
+
+  // Check for the password and email
+  if (!email || !password) {
+    throw new BadRequestError("Please provide login and password")
+  }
+
+  // Looking for user with given email
+  const user = await User.findOne({ email })
+
+  if (!user) {
+    throw new UnauthenticatedError("Invalid credentials")
+  }
+
+  // Checking the password with method created in schema
+  const isPasswordCorrect = await user.comparePassword(password)
+
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid credentials")
+  }
+
+  // Create token and send it to the client
+  const token = user.createJWT()
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
 }
 
 module.exports = { register, login }
--- models/User.js
+++ models/User.js
@@ -19,7 +19,6 @@ const UserSchema = new mongoose.Schema({
     ],
     unique: true,
   },
-
   password: {
     type: String,
     required: [true, "Please provide password"],
@@ -36,8 +35,18 @@ UserSchema.pre("save", async function (next) {
 // There is a possibility to create custom method for our document.
 // So instead writing logic of signing of jwt token in controller I`ll write it here.
 UserSchema.methods.createJWT = function () {
-  return jwt.sign({ userId: this._id, name: this.name }, "jwtSecret", {
-    expiresIn: "30d",
-  })
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  )
 }
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password)
+  return isMatch
+}
+
 module.exports = mongoose.model("User", UserSchema)
