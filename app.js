c0194811e7c8dad2084b5f693d07d404b9e32093
DenysLygach <denisligach98@gmail.com>
2023-07-04 15:23:44 +0300

    security packeges added

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ require("dotenv").config()
 require("express-async-errors")
 const express = require("express")
 const connectDB = require("./db/connect")
+
+// extra security packages
+const helmet = require("helmet")
+const cors = require("cors")
+const xss = require("xss-clean")
+const rateLimit = require("express-rate-limit")
+
 const authenticateUser = require("./middleware/authentication")
 
 const app = express()
@@ -15,7 +22,12 @@ const notFoundMiddleware = require("./middleware/not-found")
 const errorHandlerMiddleware = require("./middleware/error-handler")
 
 // extra packages
+app.set("trust proxy", 1)
+app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }))
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 app.use("/api/v1/auth", authRouter)
